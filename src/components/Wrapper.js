cb9180f46994e816835ac8f87c60f23d0700950b
Thang Le <thangbiet17@gmail.com>
2023-01-11 14:17:56 +0700

    Update Wrapper.js

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -10,8 +10,13 @@ export function Wrapper({ children }) {
     marginLeft: "0.5rem",
     display: "inline-block",
   };
+
+  const bgStyle = {
+    background: `url(${bg}) no-repeat`,
+    backgroundSize: "cover",
+  };
   return (
-    <div className="bg-gray-100 h-screen" style={{ background: `url(${bg})`}}>
+    <div className="bg-gray-100 h-screen" style={bgStyle}>
       <img src={logo} alt="logo" style={style} />
       <div className="w-8/12 mx-auto mt-10">{children}</div>
     </div>
